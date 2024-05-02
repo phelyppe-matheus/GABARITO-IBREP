handle wrong values

server.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-import json
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS, cross_origin
 import numpy as np
 
 from ORMGabarito import AnswerSheetRecognitionModel
@@ -7,15 +7,23 @@ from ORMGabarito import AnswerSheetRecognitionModel
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/api/exam/review", methods=["POST"])
+@cross_origin(origins="*")
 def exam_review():
+    res = dict()
     exam = request.json
-    reviewer = AnswerSheetRecognitionModel(exam["correctAnswers"], exam["choicesCount"])
+    questionCount = exam["questionCount"]
+    choiceCount = exam["choicesCount"]
+    reviewer = AnswerSheetRecognitionModel(questionCount, choiceCount)
     reviewer.recognise(base64=exam["examPhoto"])
-    reviewer.reviewAnswers()
-    return json.dumps({
-        "score": reviewer.score,
-        "answers": np.char.mod("%c", reviewer.studentsAnswers+65).tolist()
-    })
+    if hasattr(reviewer, "studentsAnswers"):
+        reviewer.kernelSize = 1
+        res["answers"] = np.char.mod("%c", reviewer.studentsAnswers+65).tolist()
+    else:
+        res['err'] = "Não pude reconhecer a imagem"
+    if "correctAnswers" in exam:
+        reviewer.reviewAnswers(exam["correctAnswers"])
+        res["score"] = reviewer.score,
+    return jsonify(res)
 
 @app.route("/exam/review/test", methods=["GET"])
 def test_exam_review():
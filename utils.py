94809a5942ac10968bd266b78d6bc6359ace95a4
phelyppe-matheus <phelyppemath@outlook.com>
2025-01-15 19:09:13 -0300

    removing clunky dicts

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
-def qrdata_to_examdata(qrdata):
-    examdata = {}
-    examdata["choicesCount"] = qrdata["c"]
-    examdata["questionCount"] =  qrdata["q"]
-    examdata["correctAnswers"] = qrdata["r"]
+import collections.abc
 
-    return examdata
+def qrdata_to_examdata(qrdata):
+    qrdata["c"] = int(qrdata["c"])
+    qrdata["q"] = int(qrdata["q"])
+
+    return qrdata["c"], qrdata["q"], qrdata["r"]
 
 def qrdata_to_resdata(qrdata):
     resdata = {}
@@ -15,10 +15,19 @@ def qrdata_to_resdata(qrdata):
     return resdata
 
 
-def map_answer_to_letter(answer_num, choices_count):
+def map_answer_to_letter(answerNum, choicesCount):
     """
     Mapeia um número (1, 2, 3, ...) para a respectiva letra baseada no número de opções.
-    Exemplo: Se choices_count = 5, mapeia 1 -> 'A', 2 -> 'B', 3 -> 'C', 4 -> 'D', 5 -> 'E'.
+    Exemplo: Se choicesCount = 5, mapeia 1 -> 'A', 2 -> 'B', 3 -> 'C', 4 -> 'D', 5 -> 'E'.
     """
     # Calcula a letra de acordo com o número de alternativas
-    return chr(65 + (answer_num - 1) % choices_count)  # Modulo para garantir o ciclo correto
+    print(answerNum, chr(65 + (answerNum) % choicesCount))
+    return chr(65 + (answerNum) % choicesCount)  # Modulo para garantir o ciclo correto
+
+def update(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d

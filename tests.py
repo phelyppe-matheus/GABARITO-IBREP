a2346277fd31d33ea4bf390d565f6a424952ee3f
phelyppe-matheus <phelyppemath@outlook.com>
2025-01-28 14:08:23 -0300

    testando fotos

--- tests.py
+++ tests.py
@@ -1,16 +1,31 @@
 from controller import reviewController
 
 def test_all_good():
-    exam = {
-        "examPhotoType": 3,
-        "examPhoto": "test/test_23.jpg"
+
+    tests = {
+        "test_01.jpg": ['C','C','A','D','B','A','B','C','C','A','E','C','D','A','A','B','D','A','C','C'],
+        "test_02.png": ['A','C','E','C','B','B','D','C','B','A','E','C','B','C','E','C','D','A','B','B'],
+        "test_03.png": ['A','C','E','C','B','B','D','C','B','A','E','C','B','C','E','C','D','A','B','B'],
+        "test_04.png": ['C','C','B','E','A','A','C','C','D','B','E','C','E','C','A','A','D','B','C','D'],
+        "test_05.png": ['B','C','C','C','B','D','A','C','C','B','E','C','A','A','A','B','D','B','C','B'],
+        "test_06.png": ['B','B','C','C','C','A','A','C','C','B','E','C','D','C','A','B','E','B','C','B'],
+        "test_07.png": ['B','C','C','C','C','A','D','B','D','B','E','A','D','A','A','B','D','B','C','B'],
     }
-    response = {}
 
-    reviewController(exam=exam, response=response)
+    for nome in tests:
+        exam = {
+            "examPhotoType": 3,
+            "examPhoto": "test/"+nome
+        }
+        response = {}
 
-    response["marked"] = "img"
-    print(response)
+        reviewController(exam=exam, response=response)
+
+        response["marked"] = "img"
+        if not response['answers'] == tests[nome]:
+            print(f"\n{nome}", response['answers'], tests[nome], sep="\n")
+        else:
+            print(".", end="")
 
 
 def test_test():
@@ -23,6 +38,5 @@ def test_test():
     reviewController(exam=exam, response=response)
 
     response["marked"] = "img"
-    print(response)
 
 test_all_good()

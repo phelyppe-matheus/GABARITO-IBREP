fc703d9eba69ff35483f6dbf9f47681168df4736
phelyppe-matheus <phelyppemath@outlook.com>
2025-02-03 01:55:51 -0300

    Utilizando mais variáveis do .env e de tradução

--- server.py
+++ server.py
@@ -4,12 +4,18 @@ from cryptography.fernet import Fernet
 import os
 
 
-from strings.tests import strings
+from strings.tests import strings as test_strings
+from strings.capture import strings as capture_strings
 from controller import reviewController
 
 key = os.environ.get("ANSWER_KEY")
 origin = os.environ.get("ACCEPT_ORIGINS", "*").split(",")
-ibrep_url = os.environ.get("IBREP_URL", "https://ibrep.alfamaoraculo.com.br/api/set/score")
+system_url = os.environ.get("SYSTEM_URL", "https://ibrep.alfamaoraculo.com.br/api/set/score")
+system_title = os.environ.get("SYSTEM_TITLE", "GABARITO CAMERA")
+
+COLOR_PRIMARY = os.environ.get("COLOR_PRIMARY", "red")
+COLOR_SECONDARY = os.environ.get("COLOR_SECONDARY", "blue")
+COLOR_AUX = os.environ.get("COLOR_AUX", "white")
 
 app = Flask(__name__, template_folder="templates")
 CORS(app, origins=origin)
@@ -30,14 +36,21 @@ def exam_review():
         res['err']["tipo"] = "Check the types of what you've sent."
     except ValueError as e:
         res['err']["wrongValue"] = str(e)
-    except Exception as e:
-        res['err']["unknown"] = str(e)
+    # except Exception as e:
+    #     res['err']["unknown"] = str(e)
     return jsonify(res)
 
 
 @app.route("/exam/capture", methods=["GET"])
 def exam_capture():
-    conf = {"ibrep_url": ibrep_url}
+    conf = {}
+    conf["strings"] = capture_strings
+    conf["school_url"] = system_url
+    conf["colors"] = {
+        "primary": COLOR_PRIMARY,
+        "secondary": COLOR_SECONDARY,
+        "aux": COLOR_AUX
+    }
     return render_template('capture.html', conf=conf)
 
 
@@ -45,7 +58,7 @@ def exam_capture():
 def make_qr_code():
     lang = request.accept_languages.best_match(supported_languages)
     conf = {
-        "strings": strings[lang]
+        "strings": test_strings[lang]
     }
     return render_template('makeqrcode.html', conf=conf)
 

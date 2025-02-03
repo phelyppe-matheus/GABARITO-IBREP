50e6b2b5ec899d8404000f5486ef2fd4b8aff74f
phelyppe-matheus <phelyppemath@outlook.com>
2025-02-03 01:57:17 -0300

    Traduções pt e en

---
+++ strings/capture.py
@@ -0,0 +1,10 @@
+strings = {
+    "pt": {
+        "system_title": "GABARITO CAM",
+        "overlay_alt": "Guia para foto do gabarito",
+    },
+    "en": {
+        "system_title": "ANSWER SHEET CAM",
+        "overlay_alt": "Answer sheet photo guide",
+    }
+}

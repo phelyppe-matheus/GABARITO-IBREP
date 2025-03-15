78c40f8a1147512686f38263f5b46d1bc443a92c
phelyppe-matheus <phelyppemath@outlook.com>
2025-03-15 01:22:45 -0300

    strings for null answers label

--- strings/review.py
+++ strings/review.py
@@ -2,11 +2,13 @@ strings = {
     "pt": {
         "correct_answer": "Acertou",
         "wrong_answer": "Errou",
-        "actual_answer": "Correta"
+        "actual_answer": "Correta",
+        "null_answer": "Anulada",
     },
     "en": {
         "correct_answer": "Correct",
         "wrong_answer": "Wrong",
-        "actual_answer": "Actual Answer"
+        "actual_answer": "Actual Answer",
+        "null_answer": "Null Answer",
     }
 }
